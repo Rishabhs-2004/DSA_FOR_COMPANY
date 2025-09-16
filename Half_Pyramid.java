e8c5d23b2a37627faea85ecca043e553e76c0d18
Rishabh Singh <rishabhs3011@gmail.com>
2025-09-16 17:39:50 +0530

    DSA

--- Half_Pyramid.java
+++ Half_Pyramid.java
@@ -1,11 +1,10 @@
 public class Half_Pyramid {
-    public static void main(String[] args) {
-        int n = 5;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(j+" ");
-            }
-              System.out.println();
+ public static void main(String[] args) {
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+        System.out.print(j+ " ");
         }
+        System.out.println();
     }
+ }    
 }
--- Inverted_Star.java
+++ Inverted_Star.java
@@ -1,12 +1,11 @@
 public class Inverted_Star {
-
     public static void main(String[] args) {
-        int n=5;
-        for(int i=1; i<=n; i++){
-           for(int j=1; j<=n-i+1; j++){
-            System.out.print(" * ");
-           }
-           System.out.println();
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5-i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
+        
     }
 }
