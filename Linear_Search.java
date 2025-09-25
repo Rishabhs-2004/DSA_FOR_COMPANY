d5bf36ba6e44b73a073ac8f758dbf5ccf6be9643
Rishabh Singh <rishabhs3011@gmail.com>
2025-09-25 20:48:20 +0530

    25-Aug-25

---
+++ Linear_Search.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Linear_Search {
+
+public static int LineraSearch(int num[], int key){
+    for(int i=0; i<num.length; i++){
+             if(num[i] == key){
+                return i;
+             }
+    }
+    return -1;
+        }
+
+
+
+    
+    public static void main(String[] args) {
+        int num [] = {2,4,6,8,10};
+        int key = 6;
+
+        int index = LineraSearch(num,key);
+            if(index == -1){
+                System.out.println("No is not found");
+            }else{
+                System.out.println("It is key index : " +index);
+            }
+        }
+
+    }
+
